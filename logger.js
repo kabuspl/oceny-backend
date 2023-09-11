37b931355cf70321039cd6283c9fb57bcac56790
kabuspl <kabuspl@outlook.com>
2023-09-11 15:32:10 +0200

    add sending errors to webhook

--- logger.js
+++ logger.js
@@ -1,6 +1,12 @@
 import "https://deno.land/std@0.201.0/dotenv/load.ts";
 import { red } from "https://deno.land/std@0.201.0/fmt/colors.ts";
+import * as webhookHandler from "./webhookHandler.js";
 
+/**
+ * Log text to stdin (when user configured log_level higher or equal to message's level) and file.
+ * @param {number} level - Log level. Higher number is more verbose.
+ * @param {string} content - Message.
+ */
 export function log(level, content) {
     if(level > Deno.env.get("LOG_LEVEL")) return;
     const logLine = `[${level}] [${new Date().toISOString()}]: ${content}\n`;
@@ -8,8 +14,17 @@ export function log(level, content) {
     console.log(logLine.slice(0,-1));
 }
 
+/**
+ * Log error to stdout, file and discord webhook.
+ * @param {Error} content - Error thrown by code.
+ */
 export function error(content) {
     const logLine = `[ERROR] [${new Date().toISOString()}]: ${content}\n`;
     Deno.writeTextFileSync("oceny.log", logLine, {append: true});
+    webhookHandler.sendEmbed(
+        webhookHandler.webhooks.errors,
+        content.name,
+        content.message
+    );
     console.log(red(logLine.slice(0,-1)));
 }
