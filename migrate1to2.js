88d6af68b7251e34d2ec9977ca653891dc0258f1
kabuspl <kabuspl@outlook.com>
2023-09-30 01:17:31 +0200

    add simple temporary data migrator

---
+++ migrate1to2.js
@@ -0,0 +1,81 @@
+// Simple data migrator from version 0 to 2
+const dataStoreName = Deno.args[0];
+const dataStore = JSON.parse(await Deno.readTextFile(`datastore/${dataStoreName}.json`));
+
+const newDataStore = {
+    dataStoreManifest: {
+        version: 2
+    }
+};
+
+function calculateAvg(grades) {
+    let avgTemp = 0;
+    let count = 0;
+    for(const grs in grades) {
+        const gr = parseInt(grs);
+        avgTemp+=(gr+1)*grades[gr];
+        count+=grades[gr];
+    }
+    let avg = avgTemp/count;
+    return Math.floor(avg*100)/100;
+}
+
+switch(dataStoreName) {
+    case "dayDiff": {
+        newDataStore.content = {};
+        for(const day of Object.keys(dataStore)) {
+            const dayContent = dataStore[day];
+            newDataStore.content[day] = {
+                date: dayContent.date,
+                subjects: {}
+            }
+            for(const subject of Object.keys(dayContent.subjects)) {
+                newDataStore.content[day].subjects[subject] = {
+                    grades: {
+                        ...dayContent.subjects[subject]
+                    },
+                    average: calculateAvg(dayContent.subjects[subject])
+                }
+            }
+        }
+        break;
+    }
+    case "diff": {
+        newDataStore.content = {};
+        for(const content of dataStore) {
+            let temp1 = {
+                date: content.date,
+                subjects: {}
+            }
+            let temp = {};
+            for(const subject of Object.keys(dayContent.subjects)) {
+                temp.subjects[subject] = {
+                    grades: {
+                        ...dayContent.subjects[subject]
+                    },
+                    average: calculateAvg(dayContent.subjects[subject])
+                }
+            }
+            temp1.subjects = temp;
+        }
+        break;
+    }
+    case "hisotry": {
+        newDataStore.content = {};
+        for(const day in dataStore) {
+            const dayContent = dataStore[day];
+            newDataStore.content[day] = {}
+            for(const subject in Object.keys(dayContent)) {
+                newDataStore.content[day][subject] = {
+                    grades: {
+                        ...dayContent[subject]
+                    },
+                    average: calculateAvg(dayContent[subject])
+                }
+            }
+        }
+        break;
+    }
+}
+
+await Deno.writeTextFile(`datastore/${dataStoreName}.new.json`, JSON.stringify(newDataStore));
